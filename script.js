4e2e7ae45fbf9269e7866532f74972a93df71cda
Chintan Gurjar <chintangurjar@outlook.com>
2021-04-01 04:42:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const COMMANDS = {
   certifications: 
     "OSCP - Offensive Security Certified Professional<br>CTIA - Cyber Threat Intelligence Analyst<br>CEH - Certified Ethical Hacker<br>CCFH - Certified Crowdstrike Falcon Hunter<br>CCFA - Certified CrowdStrike Falcon Administrator",
   articles:
-    "<a href='https://github.com/iamthefrogy/FYI' class='success link'>FYI - https://github.com/iamthefrogy/FYI </br> This GitHub repo has the collection of my entire career's work/research/thoughts/articles/webinar/articles/publications.</a>",
+    "<a href='https://github.com/iamthefrogy/FYI' class='success link'>FYI - https://github.com/iamthefrogy/FYI </br> This GitHub repo has the collection of my entire career's work/research/thoughts/webinars/articles/publications.</a>",
   contact:
     "You can contact me on any of following links:<br><a href='https://www.linkedin.com/in/chintangurjar/' class='success link'>Linkedin</a>, <a href='mailto:chintangurjar@outlook.com' class='success link'>Email</a>, <a href='https://twitter.com/iamthefrogy/' class='success link'>Twitter</a>"
 };
